696ae8fbb80443349b3e9264b68ca718771420f7
Ray Heberer <ray.heberer@gmail.com>
2017-08-18 14:41:20 +0200

    add geojson file

--- static/js/visualizer.js
+++ static/js/visualizer.js
@@ -71,7 +71,7 @@ function vibesPeriVibe(category, data) {
 
       "use strict";
       var margin = 30,
-          width = "80%",
+          width = "100%",
           height = 300 - margin;
 
       // filter data by date
@@ -114,4 +114,71 @@ function bindData(data_src) {
     d['vib_event_date'] = format.parse(d['vib_event_date'])
     return d;
   },draw);
+}
+
+function geo_heatmap(data_src) {
+var h = 450,
+    w = 960;
+// set-up unit projection and path
+var projection = d3.geo.mercator()
+    .scale(1)
+    .translate([0, 0]);
+var path = d3.geo.path()
+    .projection(projection);
+// set-up svg canvas
+var svg = d3.select("#map").append("svg")
+    .attr("height", h)
+    .attr("width", w);
+
+d3.json("https://rayheberer.netlify.com/js/countries.geo.json", function(error, data) {
+    d3.csv(data_src, function(error, csv) {
+        var world = data.features;
+
+        // color scale for data, starting from 0, ending at max
+        var color = d3.scale.linear()
+                      .range(["red", "blue"])
+                      .domain([0, +d3.max(csv)['Total VIBe Products Sold']])
+
+
+        // calculate bounds, scale and transform 
+        // see http://stackoverflow.com/questions/14492284/center-a-map-in-d3-given-a-geojson-object
+        var b = path.bounds(data),
+            s = .95 / Math.max((b[1][0] - b[0][0]) / w, (b[1][1] - b[0][1]) / h),
+            t = [(w - s * (b[1][0] + b[0][0])) / 2.5, (h - s * (b[1][1] + b[0][1])) / 1.3];
+        projection.scale(s+30)
+            .translate(t);
+        svg.selectAll("path")
+            .data(world).enter()
+            .append("path")
+            .style("fill", getColor)
+            .style("stroke", "grey")
+            .style("stroke-width", "1px")
+            .attr("d", path)
+            .on("mouseover", handleMouseOver)
+            .on("mouseout", handleMouseOut);
+        
+
+        // color in countries
+        function getColor(data) {
+          var value = 0;
+          csv.forEach(function(d, i) {
+            if(data.properties.name == d.Country) {
+              value = +d['Total VIBe Products Sold'];
+              return;
+            };
+          });
+          return color(value);
+        }
+
+        function handleMouseOver(d, i) {
+            d3.select(this).style("stroke-width", "1.8px");
+        }
+
+        function handleMouseOut(d, i) {
+            d3.select(this).style("stroke-width", "1px");
+        }
+debugger;
+    })
+
+  })
 }
