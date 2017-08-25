da98993c3ab846d463b15d2c94cec562a6be777b
Ray Heberer <ray.heberer@gmail.com>
2017-08-24 20:32:35 -0700

    Remove unexpected ;

--- static/js/visualizer.js
+++ static/js/visualizer.js
@@ -221,7 +221,7 @@ function grouped_bar(args) {
             l = l-1;
             elementData = elements[l].__data__
             divTooltip.html((d.category)+"<br>"+elementData.name+"<br>"+elementData.value);
-       });
+       })
        .on("mouseout", function(){
             divTooltip.style("display", "none");
        });
