4222167ae204659da2357427d4efde1a6f29b254
rayheberer <ray.heberer@gmail.com>
2018-09-15 21:30:23 -0700

    Remove social functions

--- static/megan/something.js
+++ static/megan/something.js
@@ -89,16 +89,4 @@ $(document).ready(function() {
   });
 
   $('#new-quote').on('click', getQuote);
-
-  $('#tweet-quote').on('click', function() {
-    if(!inIframe()) {
-      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-    }
-  });
-
-  $('#tumblr-quote').on('click', function() {
-    if(!inIframe()) {
-      openURL('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
-    }
-  });
 });
